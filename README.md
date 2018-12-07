4859723b102f2966cfa83cc4d5bb32a0da0c735d
Claudio <Claudio@Claudios-MacBook-Air.local>
2018-12-07 10:56:12 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio page
+
+A fun and simple webpage on which I present a few projects I've been working on. 
+
+#awanabefrontenddeveloper
---
+++ script.js
@@ -0,0 +1,15 @@
+// jQuery typed.js
+
+$(document).ready(function () {
+	var typed = new Typed('#typed', {
+		strings: ["...","HTML5...", "CSS3...", "JavaScript...", "jQuery...", "Bootstrap...", "Creativity...", "Innovation...", "Progress..."],
+		typeSpeed: 80,
+		fadeOut: true,
+		fadeOutClass: 'typed-fade-out',
+		fadeOutDelay: 500,
+		loop: true,
+		loopCount: Infinity,
+
+	});
+});
+
