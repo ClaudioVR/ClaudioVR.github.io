8e14a7b9106b972af53637ea239d9f1aaa79fec4
Claudio <Claudio@Claudios-MacBook-Air.local>
2019-02-17 22:39:50 +0100

    First commit new website
    A complete reworked website.
    Projects still need to be linked in

--- README.md
+++
@@ -1,5 +0,0 @@
-# Portfolio page
-
-A fun and simple webpage on which I present a few projects I've been working on. 
-
-#awanabefrontenddeveloper
--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-// jQuery typed.js
 
+// https://mattboldt.com/typed.js/
+
+// jQuery typed.js
 $(document).ready(function () {
 	var typed = new Typed('#typed', {
-		strings: ["...","HTML5...", "CSS3...", "JavaScript...", "jQuery...", "Bootstrap...", "Creativity...", "Innovation...", "Progress..."],
-		typeSpeed: 80,
-		fadeOut: true,
-		fadeOutClass: 'typed-fade-out',
-		fadeOutDelay: 500,
-		loop: true,
-		loopCount: Infinity,
+		// strings: ["HTML5...", "CSS3...", "JavaScript...", "jQuery...", "Bootstrap...", "Vue.js...", "and learning..."],
+		// typeSpeed: 80,
+    // backspeed: 0,
+		// fadeOut: true,
+		// fadeOutClass: 'typed-fade-out',
+		// fadeOutDelay: 500,
+		// loop: true,
+		// loopCount: Infinity,
+
+    strings: ['I work with HTML...', 'CSS... ', 'JavaScript... ', 'Vue.js... ', 'Bootstrap... ', 'and jQuery... '],
+    typeSpeed: 40,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
 
 	});
 });
-
