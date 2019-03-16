e690192b641d54d91318281a9ae18ccbee54b095
Claudio <Claudio@Claudios-MacBook-Air.local>
2019-03-16 10:47:15 +0000

    UPdate

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // jQuery typed.js
 $(document).ready(function () {
 	var typed = new Typed('#typed-1', {
-    strings: ['I\'m a Front-End Developer...', 'and I code out of Oslo, Norway...','I enjoy coding with: HTML...', 'I enjoy coding with: CSS...', 'I enjoy coding with: and JavaScript...', 'I also like using framerworks...', 'I also like using and libraries...', 'like jQuery...', 'like and Bootsrap...'],
+    strings: ['I\'m a Front-End Developer...', 'and I code out of Oslo, Norway...','I enjoy coding with: HTML...', 'I enjoy coding with: CSS...', 'I enjoy coding with: and JavaScript...', 'I also like using frameworks...', 'I also like using and libraries...', 'like Vue.js...', 'like jQuery...', 'like and Bootstrap...'],
 		startDelay: 1000,
     typeSpeed: 50,
     backSpeed: 40,
@@ -23,7 +23,7 @@ $(document).ready(function () {
 	var typed2 = new Typed('#typed-introB', {
     strings: ['I am currently looking to join a creative web team as a junior front-end developer. I look forward to challenging myself and learning loads more about the constantly changing world of front-end.'],
 		startDelay: 4000,
-    typeSpeed: 25,
+    typeSpeed: 20,
     backSpeed: 0,
     smartBackspace: true, // this is a default
     loop: false,
